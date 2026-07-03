fa3e70c02c715ec3d003dd332507ad8e0f540304
rajatasharma-gif <rajat.a.sharma@kipi.ai>
2026-07-03 10:56:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,9 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 # session = get_active_session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 name_on_order = st.text_input("Name On Smoothie")
 st.write("The name on smoothie will be ", name_on_order)
